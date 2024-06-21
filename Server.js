b1dc570d4b832fc491234ab632b30875a84f390e
diogo939405 <diogo.favilla@gmail.ccom>
2024-06-21 13:24:17 -0300

    ok

--- Server.js
+++ Server.js
@@ -24,14 +24,6 @@ app.use(bodyParser.json())
 app.use('/Routes', router);
 connectToDb()
 
-function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-        .replace(/[xy]/g, function (c) {
-            const r = Math.random() * 16 | 0,
-                v = c == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        })
-}
 
 
 async function validarProduto(idProduto) {
