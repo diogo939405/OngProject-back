beacc30daa3ee60f49912c028fe0435298ca0515
diogo939405 <diogo.favilla@gmail.ccom>
2024-07-01 10:20:41 -0300

    segunda

--- Server.js
+++ Server.js
@@ -4,13 +4,10 @@ const router = require('express')
 const cors = require('cors')
 const mongodb = require('mongodb')
 const bodyParser = require('body-parser')
-const payPal = require('paypal-rest-sdk');
 const ejs = require('ejs')
 const mongoose = require('mongoose')
 const DataModel = require('./DataModel')
-const paymentModel = require('./PaymentData')
 const connectToDb = require('./Database')
-const paypal = require('./services/Paypal')
 
 
 const app = express();
@@ -25,46 +22,6 @@ app.use('/Routes', router);
 connectToDb()
 
 
-
-async function validarProduto(idProduto) {
-    // pesquisar como validar um id no MONgoDB,vindo da chamada req.body.id
-    const resultado = await DataModel.findById(idProduto)
-        .then(userFound => {
-            // console.log(userFound)
-            return userFound ? true : false
-        })
-    return resultado
-}
-
-async function gravar(valorProduct) {
-    //procurar rotina de inclusão de dados no MongoDB
-    const payModel = await paymentModel.create(valorProduct)
-    return payModel
-}
-
-app.post('/compra', async (req, res) => {
-    try {
-        // if (validarProduto(req.body.cardId)) {
-        //     req.body.id = uuidv4();
-        //     gravar(req.body)
-        //     // gerarToken(req.body)
-        //     res.status(200).json(req.body)
-        //     console.log('passou')
-        // }
-    } catch (err) {
-        console.log(err)
-    }
-})
-
-app.get('/compra', async (req, res) => {
-    try {
-        const paydescription = await paymentModel.find({})
-        res.status(200).json(paydescription)
-    } catch (error) {
-        res.status(500)
-        console.log('deu ruim no get pay')
-    }
-})
 
 app.get('/todosDados', async (req, res) => {
     try {
